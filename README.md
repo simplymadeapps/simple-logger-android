3ce985fd5d2b99c1fb57ca69481d058054cf5551
Stephen Ruda <stephen@simplymadeapps.com>
2017-07-25 14:48:14 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# simple-logger
+A simple logger for Android that uploads to Amazon S3.
---
+++ src/main/java/com/simplymadeapps/simple_logger_android/SimpleAmazonLogs.java
@@ -0,0 +1,14 @@
+package com.simplymadeapps.simple_logger_android;
+
+public class SimpleAmazonLogs {
+
+    // Constructor
+    public SimpleAmazonLogs() {
+
+    }
+
+    // Do something
+    public void doSomething() {
+
+    }
+}
